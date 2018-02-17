3e3b66b978a5d9c7a2613f5003eb2c6f32ddb697
Lewis <lewismcduffie@gmail.com>
2018-02-16 23:27:31 -0500

    an answer

--- home_work/99_bottles_of_beer.js
+++ home_work/99_bottles_of_beer.js
@@ -2,3 +2,16 @@
 // Write a script that prints the lyrics to "99 Bottles of Beer on the Wall" in the terminal
 // Make sure your program can handle both singular and plural cases (i.e. both "99 bottles of beer" and "1 bottle of beer")
 // Hint: you will want to use a for loop that starts at 99 and counts down to 0
+//for (i = 99; i >= 0; i --)
+var one = (" bottle ");
+for (beer = 99; beer >= 0; beer--) 
+  if(beer >=2){
+    console.log(beer + " bottles of beer on the wall. " + beer + " bottles of beer. Take one down and pass it around, " + (beer-1) 
+      + " bottles of beer on the wall.");
+  }
+  else if (beer === 1) {
+    console.log(beer + one + " of beer on the wall. " + beer + one + " of  beer. Take one down and pass it around, " + (beer-1) + " bottles of beer on the wall."); 
+  } 
+  else if(beer === 0); {
+    console.log((beer+1) + " bottles of beer on the wall. " + (beer+1) + " bottles of beer. Go to the store and buy some more. 99 bottles of beer on the wall."); 
+  }
