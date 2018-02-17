6582a97ee66bb54e3360b2dfe8429b489d43c26f
Lewis <lewismcduffie@gmail.com>
2018-02-17 16:07:00 -0500

    change oddities

--- home_work/99_bottles_of_beer.js
+++ home_work/99_bottles_of_beer.js
@@ -4,7 +4,7 @@
 // Hint: you will want to use a for loop that starts at 99 and counts down to 0
 //for (i = 99; i >= 0; i --)
 var one = (" bottle ");
-for (beer = 99; beer >= 0; beer--) 
+for (var beer = 99; beer >= 0; beer--) 
   if(beer >=2){
     console.log(beer + " bottles of beer on the wall. " + beer + " bottles of beer. Take one down and pass it around, " + (beer-1) 
       + " bottles of beer on the wall.");
@@ -12,6 +12,6 @@ for (beer = 99; beer >= 0; beer--)
   else if (beer === 1) {
     console.log(beer + one + " of beer on the wall. " + beer + one + " of  beer. Take one down and pass it around, " + (beer-1) + " bottles of beer on the wall."); 
   } 
-  else if(beer === 0); {
+  else (beer === 0); {
     console.log((beer+1) + " bottles of beer on the wall. " + (beer+1) + " bottles of beer. Go to the store and buy some more. 99 bottles of beer on the wall."); 
   }
